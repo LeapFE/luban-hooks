0ca24b094b24e782b414eb21e4d8b68a2dce77a1
front-end-captain <18810940932@163.com>
2020-07-24 15:40:21 +0800

    build: improve publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,31 +1,22 @@
 "use strict";
 
+// CHECK VERSION TAG
+
 const shell = require("shelljs");
+const minimist = require("minimist");
+
+const rawArgs = process.argv.slice(2);
+const args = minimist(rawArgs);
 
 if (!shell.exec("npm config get registry").stdout.includes("https://registry.npmjs.org/")) {
   console.error("Failed: set npm registry to https://registry.npmjs.org/ first");
   process.exit(1);
 }
 
-const ret = shell.exec("./node_modules/.bin/lerna updated").stdout;
-const updatedRepos = ret
-  .split("\n")
-  .map((line) => line.replace("- ", ""))
-  .filter((line) => line !== "");
+const publishCommand = `lerna publish from-git --yes --dist-tag ${args.tag}`;
 
-if (updatedRepos.length === 0) {
-  console.log("No package is updated.");
-  process.exit(0);
-}
-
-const { code: versionCode } = shell.exec("lerna version --exact --yes --conventional-commits");
-if (versionCode === 1) {
-  console.error("Failed: lerna version --exact --yes --conventional-commits");
-  process.exit(1);
-}
-
-const { code: publishCode } = shell.exec("lerna publish from-git --yes");
+const { code: publishCode } = shell.exec(publishCommand);
 if (publishCode === 1) {
-  console.error("Failed: lerna publish from-git --yes");
+  console.error(`Failed: ${publishCommand}`);
   process.exit(1);
 }
